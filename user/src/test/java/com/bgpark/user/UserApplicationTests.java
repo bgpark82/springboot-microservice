19a22ad7526b1382bfce3460c5b533d35f39f19c
peter-alethio <peter@alethio.io>
2022-08-27 14:04:52 +0900

    feat: 유레카 서비스 생성

---
+++ user/src/test/java/com/bgpark/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bgpark.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
